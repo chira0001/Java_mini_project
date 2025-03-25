d0155a4d1df7d3c52b19989c50ae312c60d3e0f5
JMPBWJ <pasinduunivercity28@gmail.com>
2025-03-26 01:39:52 +0530

    GUI design

---
+++ System/TecSys/src/DeleteUser.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class DeleteUser {
+    private JPanel DeleteUser;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton cancelButton;
+    private JButton deleteButton;
+}
---
+++ System/TecSys/src/Login.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Login {
+    private JPanel Login;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton loginButton;
+    private JButton button1;
+}
